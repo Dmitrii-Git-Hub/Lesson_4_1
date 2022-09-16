6fb3e07a3a79d93f48912453ef5513f31800122f
Dima <nhr@bk.ru>
2022-09-16 13:13:44 +0700

    000

---
+++ 000/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (i == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (i == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (i == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (i == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (i == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (i == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
