6d8934b609a359b4f2135cfbf28d1e11f1a56297
Dima <nhr@bk.ru>
2022-09-16 13:47:28 +0700

    002

---
+++ 002/Program.cs
@@ -0,0 +1,17 @@
+﻿// "...и выдаёт, какое число большее, а какое меньшее." (Значит выдавать два числа!)
+
+Console.WriteLine("Введите первое число");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (max > i)
+{
+    Console.WriteLine(max + " большее число, " + i + " меньшее число.");
+}
+
+if (max < i)
+{
+    Console.WriteLine(i + " большее число, " + max + " меньшее число.");
+}
